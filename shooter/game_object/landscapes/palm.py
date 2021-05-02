3a3170230c19a63733f3487b8c551c19d1b6e6dd
codershkoder <fursik111@mail.ru>
2021-05-02 21:25:29 +0300

    Исправлена опечатка

--- shooter/game_object/landscapes/palm.py
+++ shooter/game_object/landscapes/palm.py
@@ -2,7 +2,7 @@ from shooter.game_object.landscape import Landscape
 from shooter.config import LANDSCAPE_PALM
 
 
-class Ground(Landscape):
+class Palm(Landscape):
     """Класс пальмы"""
     def __init__(self, x, y, image=LANDSCAPE_PALM):
         super().__init__(x, y, image)
